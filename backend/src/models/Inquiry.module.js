24b4fcd8b04a20580db3e0301e329f97ff895e12
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-07-03 01:22:25 +0530

    feat: add Inquiry model with schema for handling inquiries

---
+++ backend/src/models/Inquiry.module.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const inquirySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    phone: {
+        type: String,
+        required: [true, "Phone number is mandatory"],
+        trim: true
+    },
+    company: {
+        type: String,
+        default: 'Individual Buyer',
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    status:{
+        type: String,
+        enum: ['Pending', 'Quoted', 'Archived'],
+        default: 'Pending',
+    }
+},{timestamps: true});
+
+export const Inquiry = mongoose.model('Inquiry', inquirySchema);
