1ac628c701f087d558d01e0d09f89c77f639c11c
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-07-13 15:05:33 +0530

    fix: remove empty title fields from product data

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -12,16 +12,16 @@ export const principleCompanies = [
 export const productsData = {
   "MYK Laticrete":{
     products:[
-      { id: "f-1", title: "", image: "/ProductsLogo/MYK/1.png" },
-      { id: "f-2", title: "", image: "/ProductsLogo/MYK/2.png" },
-      { id: "f-3", title: "", image: "/ProductsLogo/MYK/3.png" },
-      { id: "f-4", title: "", image: "/ProductsLogo/MYK/4.png" },
-      { id: "f-5", title: "", image: "/ProductsLogo/MYK/5.png" },
-      { id: "f-6", title: "", image: "/ProductsLogo/MYK/6.png" },
-      { id: "f-7", title: "", image: "/ProductsLogo/MYK/7.png" },
-      { id: "f-8", title: "", image: "/ProductsLogo/MYK/8.png" },
-      { id: "f-9", title: "", image: "/ProductsLogo/MYK/9.png" },
-      { id: "f-10", title: "",image: "/ProductsLogo/MYK/10.png"},
+      { id: "f-1", image: "/ProductsLogo/MYK/1.png" },
+      { id: "f-2", image: "/ProductsLogo/MYK/2.png" },
+      { id: "f-3", image: "/ProductsLogo/MYK/3.png" },
+      { id: "f-4", image: "/ProductsLogo/MYK/4.png" },
+      { id: "f-5", image: "/ProductsLogo/MYK/5.png" },
+      { id: "f-6", image: "/ProductsLogo/MYK/6.png" },
+      { id: "f-7", image: "/ProductsLogo/MYK/7.png" },
+      { id: "f-8", image: "/ProductsLogo/MYK/8.png" },
+      { id: "f-9", image: "/ProductsLogo/MYK/9.png" },
+      { id: "f-10",image: "/ProductsLogo/MYK/10.png"},
     ]
   },
 }
