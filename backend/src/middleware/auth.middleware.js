d5e7bec2f75fa09453b9685103fd06300f490859
tejas-tj29 <btech10225.25@bitmesra.ac.in>
2026-07-15 20:03:49 +0530

    feat: add JWT verification middleware for admin authentication

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import {Admin} from "../models/admin.module.js";
+import asyncHandler from "../utils/AsyncHandler.js";
+import ApiError from "../utils/ApiErrorHandler.js";
+
+export const verifyJWT = asyncHandler(async (req,_,next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","") ;
+        
+        if(!accessToken){
+            throw new ApiError(401, "Unauthorized request, access token is missing");
+        }
+    
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    
+        const admin = await Admin.findById(decodedToken?._id).select("-password -refreshToken");
+        
+        if(!admin){
+            throw new ApiError(401, "Unauthorized request, admin not found");
+        }
+    
+        req.admin = admin;
+        next();
+    } 
+    catch (error) {
+        throw new ApiError(401, error?.message || "Unauthorized request, invalid access token");
+    }
+
+});
